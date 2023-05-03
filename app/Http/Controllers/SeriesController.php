79840d598cbe7d2df3ab2ff9218a1a0c4708aa37
Your Name <ti.rafaelteles@gmail.com>
2023-05-02 21:06:15 -0300

    blade

--- app/Http/Controllers/SeriesController.php
+++ app/Http/Controllers/SeriesController.php
@@ -16,4 +16,9 @@ class SeriesController extends Controller
 
         return view('series.index')->with('series', $series);
     }
+
+    public function create()
+    {
+        return view('series.create');
+    }
 }
